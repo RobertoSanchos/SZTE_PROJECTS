2058a0def0898fde62b7d369efd05f4cbef20ab4
Sanchos <roberto19910619@gmail.com>
2025-04-09 12:26:07 +0200

    .

--- ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Start_activity.java
+++ ANDROID/TechBazaar/app/src/main/java/hu/techbazaar/Start_activity.java
@@ -3,10 +3,10 @@ package hu.techbazaar;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -24,6 +24,7 @@ public class Start_activity extends AppCompatActivity {
 
     EditText login_email, password;
     CheckBox remember_check;
+    TextView forgotPassword = findViewById(R.id.forgot_password);
 
     private FirebaseAuth Main_Auth;
 
